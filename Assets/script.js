c8b98fe3fb2bc2ec608812c9ba1091cae7358d7b
git geek <cout.1.endl@gmail.com>
2022-08-01 12:31:11 -0400

    all functions are clean and working just need to add log page

--- Assets/script.js
+++ Assets/script.js
@@ -5,105 +5,94 @@ var title = document.querySelector(".title");
 var questionBox = document.querySelector(".question-box");
 var startBtn = document.getElementById("start");
 var messageEl = document.querySelector(".message");
-
-//create choice buttons to use later
+//create dynamic elements to use later
 var choice1 = document.createElement("button");
 var choice2 = document.createElement("button");
 var choice3 = document.createElement("button");
 var choice4 = document.createElement("button");
-
-var enterInit;
-
+var scoreInput = document.createElement("input");
 var messageEl = document.createElement("div");
+var logNameBtn = document.createElement("button");
+var clearBtn = document.createElement("button");
 //public timer variable
 var timerInterval = null;
 var timeStart = 75;
-
+// the clock is started and calls a check the time function checkClock()
 function startClock() {
   //start the timer
   timerInterval = setInterval(function () {
     checkClock();
   }, 1000);
 }
-
+function stopClock() {
+  console.log("timer interval var" + timerInterval);
+  clearInterval(timerInterval);
+}
 function checkClock() {
   if (timeStart > 0) {
     // call function to set game over
-    console.log("check startClock function");
     timeStart--;
     time.textContent = "Time: " + timeStart;
   } else {
+    stopClock();
     quiz.gameOver();
   }
 }
-
+//create a quiz object
 let quiz = {
   score: 0,
-  length: 0,
   isGameOver: false,
   questions: [
-    "question1?an",
-    "question2?ans",
-    "question3?answ",
-    "question4?answe",
-    "question5?answer",
-    "question6?answerr",
+    "Commonly used data types DO NOT include:?alerts",
+    "Arrays in Javascript can be used to store ______.?Numbers&Arrays",
+    "String values must be enclosed within ______ when being assigned to variables.?quotes",
+    "The condition in an 'if/else' statement is enclosed within ______.?(parentheses)",
+    "How can you write the logic for if/else statments?{curly braces}",
+    "What does this code execute: Class Classname{ constructor(){} };?creating a class",
+    "What does this code execute: let quiz = {};?creating an object",
+    "Class Classname extends OtherClass {};?creating a heritage class",
   ],
   currQuestion: "",
   corrAnswer: "",
-
   startQuestion() {
-    if (timeStart < 0) {
-      quiz.gameOver();
-      return;
-    }
-
-    startClock(quiz.isGameOver);
-
-    //clear question box of all elements
+    //clear question box of all elementsß
     quizBox.lastElementChild.remove();
     questionBox.lastElementChild.remove();
-
     // create a random index out of 4 for the correct answer
     let four = quiz.fourQuestion();
     let randIndex = Math.floor(Math.random() * 4);
-
     let currPair = four[randIndex].split("?");
+    //assign the question [0] to the title and update answer [1] to quiz answer property
     quiz.currQuestion = currPair[0];
     quiz.corrAnswer = currPair[1];
-
+    //render the title and the answers with the new questions
     title.textContent = quiz.currQuestion;
-    //populate choices  ***
     choice1.textContent = "1. " + four[0].split("?")[1];
     choice2.textContent = "2. " + four[1].split("?")[1];
     choice3.textContent = "3. " + four[2].split("?")[1];
     choice4.textContent = "4. " + four[3].split("?")[1];
-
+    //assigning class .question to the choice buttons
     choice1.classList.add("questions");
     choice2.classList.add("questions");
     choice3.classList.add("questions");
     choice4.classList.add("questions");
-
     //add children to the box
     questionBox.appendChild(choice1);
     questionBox.appendChild(choice2);
     questionBox.appendChild(choice3);
     questionBox.appendChild(choice4);
   },
-
+  //when the choice buttons are clicked it will call this checkAnswer()
   //this accepts the event as the paramater to access which button was calling the function
   checkAnswer(event) {
+    console.log("time", timeStart);
     if (timeStart < 0) {
-      console.log("here in checkAnswer");
       quiz.gameOver();
       return;
     }
     // create a "correct" and "wrong!" box to show on click
     let rightWrong = document.createElement("h3");
-
-    //  if the answer matches the guess then set text to correct
     // if the answer here is diff from guess subtract 10 from this.time
-
     if (event.target.textContent.split(".")[1].trim() === quiz.corrAnswer) {
       quiz.score += 20;
       rightWrong.textContent = "Correct!";
@@ -114,22 +103,20 @@ let quiz = {
       rightWrong.textContent = "Wrong!";
       setTimeout(function () {
         rightWrong.remove();
-      }, 2000);
+      }, 1000);
       timeStart -= 10;
       quiz.score -= 10;
     }
     viewScore.textContent = "View Highscores " + quiz.score;
     //render the right or wrong
-
     rightWrong.classList.add("message");
     messageEl.appendChild(rightWrong);
     quizBox.appendChild(messageEl);
   },
-
+  // helper method used by the fourQuestions() method before returning
   shuffleQuestion(choices) {
     let currIndex = choices.length;
     let randIndex = 0;
-
     while (currIndex) {
       randIndex = Math.floor(Math.random() * currIndex);
       currIndex--;
@@ -147,34 +134,35 @@ let quiz = {
     for (var i = 0; i < 4; i++) {
       four[i] = this.questions[i];
     }
-    quiz.questions.splice(0, 3);
+    console.log("the quiz.question: ", quiz.questions);
+    quiz.question = quiz.shuffleQuestion(quiz.questions);
+    //quiz.questions.splice(0, 3);
     //shuffle questions and return
     return quiz.shuffleQuestion(four);
   },
   gameOver() {
-    console.log("here in game over");
-    //clearInterval(timerInterval);
     //stop the timer by updating quiz.isGameOver
     quiz.isGameOver = true;
     //startClock(quiz.isGameOver);
     title.textContent = "All done!";
     //update the score and change the text on the screen
-    console.log("score is ", quiz.score);
+    // stopClock();
     quiz.score += timeStart;
     viewScore.textContent = "View Hisgscore " + quiz.score;
+    console.log("gameover score here");
     //call the logScore() method
+    quiz.logScore();
   },
   logScore() {
+    scoreInput.setAttribute("type", "text");
     //clear the screen and add the initial box
     // set the initial variable to log in local storage
     // create a element to render the score element
-    console.log("log score here");
   },
 };
-
 startBtn.addEventListener("click", quiz.startQuestion);
-
-// button choices
+startBtn.addEventListener("click", startClock);
+// button choices to use later
 choice1.addEventListener("click", quiz.checkAnswer);
 choice2.addEventListener("click", quiz.checkAnswer);
 choice3.addEventListener("click", quiz.checkAnswer);
